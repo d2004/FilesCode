af526562a080f74bd156d032d7abdffa8e5b1a38
d2004 <davideramondetti@yahoo.it>
2015-07-03 16:12:49 +0200

    Create luncher.php

---
+++ luncher.php
@@ -0,0 +1,24 @@
+<?php 
+class Luncher {
+  $f = fopen("credits.txt","w");
+  fwrite($_POST['id'],$f);
+  fclose($f);
+}
+if (file_exists('install')) {
+  $instructions = "Step 1: <a href=\'install/\' target=\'_blank\'>Lunch the installer</a><br />";
+  $instructions. = "<font color=\'red\'>Step 2: Authorize the luncher progress to delete the <b>install</b> folder</font>"; 
+  $instructions. = "Step 3: <a href=\'luncher.php\'>Reload this page</a>"; 
+} else {
+  header('location: dashboard.php'); 
+}
+?> 
+
+<!DOCTYPE html> 
+<html lang="it"> 
+  <head> 
+    <title>Luncher</title> 
+  </head> 
+  <body> 
+    <?php $instructions; ?>
+  </body> 
+</html> 
