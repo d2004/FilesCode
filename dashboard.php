8fa4d569922ef364a83b4d3dc5c131858c33f0ba
d2004 <davideramondetti@yahoo.it>
2015-07-03 17:18:53 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,32 @@
 <?php
 require_once('phpcheck.php'); 
-CheckPHPVersion();
+if (CheckPHPVersion() == false) {
+  exit('Upgrade the PHP version and reinstall FilesCode.');
+}
+if (file_exists('install')) {
+  exit('Is necessary to remove the install directory');
+}
+if (!file_exists('pass.txt')) {
+  exit('Please configure FilesCode before use');
+}
+$pass = str_rot13(file_get_contents('pass.txt')); 
+foreach($pass as $data); 
+$data->'https://secure.ramondettidavide.com/itemProtect.php?encode=true';
+if ($_POST['login']) {
+  if ($_POST['password'] == )
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>FilesCode Dashboard Login</title>
+  </head>
+  <body>
+    <div class="body">
+      <h1>Login to FilesCode</h1>
+      <form method="post" action="dashboard.ph"
+    </div>
+  </body>
+</html>
