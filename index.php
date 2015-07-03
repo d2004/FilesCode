b291fadab11961c840837e5661102d966e4cf6f2
d2004 <davideramondetti@yahoo.it>
2015-07-03 16:22:28 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+if (phpversion() < 5.6.10) {
+  echo "Step 1: Go to <b><a href=\'http://php.net/downloads\' target=\'_blank\'>php.net/downloads</a></b> <br /> ";
+  echo "Step 2: <b>Install the last version of PHP</b><br />";
+  echo "Step 3: <a href=\'install/\'>Reload this Page</a><br />";
+  echo "<h1>PHP Info</h1>";
+  phpinfo();
+} else {
+  header('location: step1.php');
+}
+?>
